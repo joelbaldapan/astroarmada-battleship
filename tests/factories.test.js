6dc143f1205c8f4d6d5fd8d0ccb036e1723295f4
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-07-15 23:18:19 +0800

    fix: update tests for Gameboard class

--- tests/factories.test.js
+++ tests/factories.test.js
@@ -1,4 +1,5 @@
 const Ship = require("../src/factories/shipFactory");
+const Gameboard = require("../src/factories/gameboardFactory");
 
 describe("Ship Class", () => {
   let ship;
@@ -57,60 +58,85 @@ describe("Ship Class", () => {
 describe("Gameboard", () => {
   let board;
   beforeEach(() => {
-    const board = new board(3, 3);
+    board = new Gameboard(3, 3);
+    board.resetBoard();
   });
 
   it("should create intialize a gameboard with isShip and isHit properties", () => {
-    const board = new gameboard(2, 2); // Two by two grid
-    expect(board.coordinates).toBe([
+    expect(board.coordinates).toEqual([
       [
-        { hasHit: false, hasShip: false },
-        { hasHit: false, hasShip: false },
+        { hasHit: false, hasShip: null },
+        { hasHit: false, hasShip: null },
+        { hasHit: false, hasShip: null },
       ],
       [
-        { hasHit: false, hasShip: false },
-        { hasHit: false, hasShip: false },
+        { hasHit: false, hasShip: null },
+        { hasHit: false, hasShip: null },
+        { hasHit: false, hasShip: null },
+      ],
+      [
+        { hasHit: false, hasShip: null },
+        { hasHit: false, hasShip: null },
+        { hasHit: false, hasShip: null },
       ],
     ]);
   });
 
   it("should place ships correctly, horizontally", () => {
     board.placeShip([1, 0], 2, "horizontal"); // Location, Length, Rotation
-    expect(board.coordinates[1][0].hasShip).toBe(ship);
-    expect(board.coordinates[1][1].hasShip).toBe(ship);
+
+    const expectedShip = new Ship(2); // Replica ship
+
+    expect(board.coordinates[1][0].hasShip).toEqual(expectedShip);
+    expect(board.coordinates[1][1].hasShip).toEqual(expectedShip);
   });
 
   it("should place ships correctly, vertically", () => {
     board.placeShip([0, 2], 3, "vertical"); // Location, Length, Rotation
-    expect(board.coordinates[0][2].hasShip).toBe(ship);
-    expect(board.coordinates[1][2].hasShip).toBe(ship);
-    expect(board.coordinates[2][2].hasShip).toBe(ship);
+
+    const expectedShip = new Ship(3); // Replica ship
+    expect(board.coordinates[0][2].hasShip).toEqual(expectedShip);
+    expect(board.coordinates[1][2].hasShip).toEqual(expectedShip);
+    expect(board.coordinates[2][2].hasShip).toEqual(expectedShip);
+  });
+
+  it("should have validPlacement() method to check if ship is placed on another ship", () => {
+    board.placeShip([1, 0], 3, "horizontal");
+
+    expect(board.validPlacement([1, 0], 3, "vertical")).toBe(false);
+  });
+
+  it("should have validPlacement() method to check if ship is placed out of bounds", () => {
+    expect(board.validPlacement([3, 3], 3, "vertical")).toBe(false);
   });
 
   it("should have receiveAttack() that takes coordinates, then records the shot", () => {
-    board.receiveAttack([1, 2]);
+    board.receiveAttack(1, 2);
     expect(board.coordinates[1][2].hasHit).toBe(true);
   });
 
   it("should have receiveAttack() that calls the 'hit()' function of a ship if it's hit", () => {
-    board.receiveAttack([1, 2]);
-    expect(board.coordinates[1][2]).toBe("X");
+    board.placeShip([1, 0], 3, "horizontal");
+
+    board.receiveAttack(1, 0);
+    board.receiveAttack(1, 2);
+    expect(board.coordinates[1][2].hasShip.hits).toBe(2);
   });
 
   it("should have checkFinish() method to check if all ships have not sunk", () => {
     board.placeShip([1, 0], 1, "horizontal");
-    board.placeShip([0, 3], 2, "vertical");
+    board.placeShip([0, 2], 2, "vertical");
 
     expect(board.checkFinish()).toBe(false);
   });
 
   it("should have checkFinish() method to check if all ships have sunk", () => {
     board.placeShip([1, 0], 1, "horizontal");
-    board.placeShip([0, 3], 2, "vertical");
+    board.placeShip([0, 2], 2, "vertical");
 
-    board.receiveAttack([1, 0]);
-    board.receiveAttack([0, 3]);
-    board.receiveAttack([1, 3]);
+    board.receiveAttack(1, 0);
+    board.receiveAttack(0, 2);
+    board.receiveAttack(1, 2);
 
     expect(board.checkFinish()).toBe(true);
   });
