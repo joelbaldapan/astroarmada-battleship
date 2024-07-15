f71fea715930ac27d320d3449e711617a52f7bff
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-07-15 21:15:44 +0800

    test: add tests for Gameboard class

--- tests/app.test.js
+++ tests/app.test.js
@@ -54,3 +54,64 @@ describe("Ship Class", () => {
   });
 });
 
+describe("Gameboard", () => {
+  let board;
+  beforeEach(() => {
+    const board = new board(3, 3);
+  });
+
+  it("should create intialize a gameboard with isShip and isHit properties", () => {
+    const board = new gameboard(2, 2); // Two by two grid
+    expect(board.coordinates).toBe([
+      [
+        { hasHit: false, hasShip: false },
+        { hasHit: false, hasShip: false },
+      ],
+      [
+        { hasHit: false, hasShip: false },
+        { hasHit: false, hasShip: false },
+      ],
+    ]);
+  });
+
+  it("should place ships correctly, horizontally", () => {
+    board.placeShip([1, 0], 2, "horizontal"); // Location, Length, Rotation
+    expect(board.coordinates[1][0].hasShip).toBe(ship);
+    expect(board.coordinates[1][1].hasShip).toBe(ship);
+  });
+
+  it("should place ships correctly, vertically", () => {
+    board.placeShip([0, 2], 3, "vertical"); // Location, Length, Rotation
+    expect(board.coordinates[0][2].hasShip).toBe(ship);
+    expect(board.coordinates[1][2].hasShip).toBe(ship);
+    expect(board.coordinates[2][2].hasShip).toBe(ship);
+  });
+
+  it("should have receiveAttack() that takes coordinates, then records the shot", () => {
+    board.receiveAttack([1, 2]);
+    expect(board.coordinates[1][2].hasHit).toBe(true);
+  });
+
+  it("should have receiveAttack() that calls the 'hit()' function of a ship if it's hit", () => {
+    board.receiveAttack([1, 2]);
+    expect(board.coordinates[1][2]).toBe("X");
+  });
+
+  it("should have checkFinish() method to check if all ships have not sunk", () => {
+    board.placeShip([1, 0], 1, "horizontal");
+    board.placeShip([0, 3], 2, "vertical");
+
+    expect(board.checkFinish()).toBe(false);
+  });
+
+  it("should have checkFinish() method to check if all ships have sunk", () => {
+    board.placeShip([1, 0], 1, "horizontal");
+    board.placeShip([0, 3], 2, "vertical");
+
+    board.receiveAttack([1, 0]);
+    board.receiveAttack([0, 3]);
+    board.receiveAttack([1, 3]);
+
+    expect(board.checkFinish()).toBe(true);
+  });
+});
