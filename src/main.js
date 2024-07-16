8b981fe6173603df75473ab9c844ec5438139ac1
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-07-17 01:38:49 +0800

    feat: add game, event, render Controller classes

--- src/main.js
+++ src/main.js
@@ -1,9 +1,56 @@
 import Player from "./factories/playerFactory.js";
 
-const human = new Player("human");
-const computer = new Player("computer");
+class GameController {
+  constructor() {
+    this.human = new Player("human");
+    this.computer = new Player("computer");
+  }
 
-human.gameboard.resetBoard(10, 10);
-computer.gameboard.resetBoard(10, 10);
+  restartGame(height, length) {
+    this.human.gameboard.resetBoard(height, length);
+    this.computer.gameboard.resetBoard(height, length);
+  }
 
-console.log(human.gameboard.coordinates);
+  getHumanPlayer() {
+    return this.human;
+  }
+
+  getComputerPlayer() {
+    return this.computer;
+  }
+}
+
+class EventController {
+  constructor() {
+    this.restartBtn = document.getElementById("restart-btn");
+    this.consoleBtn = document.getElementById("console-btn");
+
+    // Add GameController and RenderController
+    this.gameController = new GameController();
+    this.renderController = new RenderController(this.gameController);
+
+    // Event listeners
+    this.restartBtn.addEventListener("click", () => {
+      this.gameController.restartGame(6, 6); // Adjustable height, length
+    });
+
+    this.consoleBtn.addEventListener("click", () => {
+      this.renderController.consoleLog();
+    });
+  }
+}
+
+class RenderController {
+  constructor(gameController) {
+    this.gameController = gameController;
+  }
+
+  consoleLog() {
+    const human = this.gameController.getHumanPlayer();
+    const computer = this.gameController.getComputerPlayer();
+    console.log("Human gameboard:", human.gameboard.coordinates);
+    console.log("Computer gameboard:", computer.gameboard.coordinates);
+  }
+}
+
+const eventController = new EventController();
