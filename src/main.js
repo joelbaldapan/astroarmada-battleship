1164c5b83b072b1f49eefdcd232392059287847d
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-07-18 03:36:36 +0800

    feat: add renderBoard to render gameboards on DOM

--- src/main.js
+++ src/main.js
@@ -31,11 +31,12 @@ class EventController {
 
     // Event listeners
     this.restartBtn.addEventListener("click", () => {
-      this.gameController.restartGame(6, 6); // Adjustable height, length
+      this.gameController.restartGame(10, 10); // Adjustable height, length
     });
 
     this.consoleBtn.addEventListener("click", () => {
       this.renderController.consoleLog();
+      this.renderController.renderBoard();
     });
   }
 }
@@ -43,6 +44,27 @@ class EventController {
 class RenderController {
   constructor(gameController) {
     this.gameController = gameController;
+  }
+
+  renderBoard() {
+    // Gameboard
+    const gameboardContainer = document.getElementById("gameboard-container");
+    const gameboard = document.createElement("div");
+    gameboard.className = "gameboard";
+
+    // Cells
+    const height = 10; // Adjustable height, length
+    const length = 10;
+    const totalCells = height * length;
+
+    for (let i = 0; i < totalCells; i++) {
+      const cell = document.createElement("div");
+      cell.className = "cell";
+      gameboard.appendChild(cell);
+    }
+
+    gameboard.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+    gameboardContainer.appendChild(gameboard);
   }
 
   consoleLog() {
