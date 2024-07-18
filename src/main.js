93bfbb67fc0801a9aff840a58e4999c0c47be686
joelbaldapan <baldapan.joelangelo@gmail.com>
2024-07-18 13:27:33 +0800

    feat: add updateBoard to render hits on the grid

--- src/main.js
+++ src/main.js
@@ -6,11 +6,6 @@ class GameController {
     this.computer = new Player("computer");
   }
 
-  restartGame(height, length) {
-    this.human.gameboard.resetBoard(height, length);
-    this.computer.gameboard.resetBoard(height, length);
-  }
-
   getHumanPlayer() {
     return this.human;
   }
@@ -18,12 +13,31 @@ class GameController {
   getComputerPlayer() {
     return this.computer;
   }
+
+  restartGame(height, length) {
+    this.human.gameboard.resetBoard(height, length);
+    this.computer.gameboard.resetBoard(height, length);
+  }
+
+  attack(board) {
+    let player;
+    if (board === "human") player = this.human;
+    else player = this.computer;
+
+    player.gameboard.placeShip([0, 1], 2, "horizontal");
+    player.gameboard.receiveAttack(0, 1); //temp
+    player.gameboard.receiveAttack(4, 3); //temp
+    player.gameboard.receiveAttack(10, 3); //temp
+  }
 }
 
 class EventController {
   constructor() {
     this.restartBtn = document.getElementById("restart-btn");
     this.consoleBtn = document.getElementById("console-btn");
+    this.renderBtn = document.getElementById("render-btn");
+    this.updateBtn = document.getElementById("update-btn");
+    this.attackBtn = document.getElementById("attack-btn");
 
     // Add GameController and RenderController
     this.gameController = new GameController();
@@ -36,7 +50,20 @@ class EventController {
 
     this.consoleBtn.addEventListener("click", () => {
       this.renderController.consoleLog();
-      this.renderController.renderBoard();
+    });
+
+    this.renderBtn.addEventListener("click", () => {
+      this.renderController.renderBoard("human");
+      this.renderController.renderBoard("computer");
+    });
+
+    this.updateBtn.addEventListener("click", () => {
+      this.renderController.updateBoard();
+    });
+
+    this.attackBtn.addEventListener("click", () => {
+      this.gameController.attack("computer");
+      this.renderController.updateBoard("");
     });
   }
 }
@@ -46,11 +73,46 @@ class RenderController {
     this.gameController = gameController;
   }
 
-  renderBoard() {
+  updateBoard() {
+    const gameboards = document.getElementsByClassName("gameboard");
+
+    Array.from(gameboards).forEach((board) => {
+      let player;
+      if (board.id === "human") {
+        player = this.gameController.getHumanPlayer();
+      } else {
+        player = this.gameController.getComputerPlayer();
+      }
+
+      let cellIndex = 0;
+      player.gameboard.coordinates.forEach((row) => {
+        row.forEach((cell) => {
+          this.checkUpdateCell(cell, cellIndex);
+          cellIndex++;
+        });
+      });
+    });
+  }
+
+  checkUpdateCell(cell, cellIndex) {
+    if (cell.hasHit) {
+      const imgElement = document.createElement("img");
+      imgElement.src = "/src/assets/images/gameboard/hit.png";
+      if (cell.hasShip)
+        imgElement.src = "/src/assets/images/gameboard/hit-and-ship.png";
+
+      const targetCell = document.getElementById(`cell-${cellIndex}`);
+      targetCell.innerHTML = ""; // Remove all children
+      targetCell.appendChild(imgElement);
+    }
+  }
+
+  renderBoard(player) {
     // Gameboard
     const gameboardContainer = document.getElementById("gameboard-container");
     const gameboard = document.createElement("div");
     gameboard.className = "gameboard";
+    gameboard.id = player;
 
     // Cells
     const height = 10; // Adjustable height, length
@@ -60,10 +122,12 @@ class RenderController {
     for (let i = 0; i < totalCells; i++) {
       const cell = document.createElement("div");
       cell.className = "cell";
+      cell.id = `cell-${i}`;
       gameboard.appendChild(cell);
     }
 
     gameboard.style.gridTemplateColumns = `repeat(${length}, 1fr)`;
+    gameboard.style.gridTemplateRows = `repeat(${length}, 1fr)`;
     gameboardContainer.appendChild(gameboard);
   }
 
